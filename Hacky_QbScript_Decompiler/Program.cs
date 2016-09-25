cc28f86e52d1f613707e7dc23a7edf241eef794f
Iris <Iris@DESKTOP-P19119H>
2016-09-25 02:49:55 +0100

    Add project files.

---
+++ Hacky_QbScript_Decompiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacky_QbScript_Decompiler {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
